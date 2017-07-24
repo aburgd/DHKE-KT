062c3bc6c0c977b1f213a05cb2aaf6525602859e
Alec Burgdorf <aeburgd@gmail.com>
2017-07-24 13:40:52 -0500

    Feature classes (#2)
    * add DigestTools class and methods; print key and secret digests
    
    * add Generation and LongOps classes to clean up DHKEBI_J
    
    * make DigestTools class and getDigest method package-private
    
    * add DigestPrinter to public key and shared secret cases for printing
    
    * make DigestTools package-private
    
    * fix printing and digest logic errors

---
+++ src/Generation.java
@@ -0,0 +1,47 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+class Generation {
+    private static Scanner stdin = new Scanner(System.in);
+    private static SecureRandom csprng = new SecureRandom();
+
+    // generation methods
+    static BigInteger publicKeyGeneration() {
+        BigInteger publicKey, moduloLong, baseLong, secretLong;
+        moduloLong = LongOps.longToBigInt(LongOps.getModuloLong());
+        baseLong = LongOps.longToBigInt(LongOps.getBaseLong());
+        secretLong = LongOps.longToBigInt(LongOps.getSecretLong());
+        boolean failOrSuccess = LongOps.checkFailure(moduloLong, baseLong,
+                secretLong);
+        System.out.printf("failOrSuccess: %b\n", failOrSuccess);
+        if (failOrSuccess) {
+            System.out.println("BAD_LONG: Error getting long");
+            System.exit(-1);
+            publicKey = BigInteger.valueOf(-1);
+            return publicKey;
+        } else {
+            publicKey = baseLong.modPow(secretLong, moduloLong);
+            return publicKey;
+        }
+    }
+    static BigInteger sharedSecretGeneration() {
+        BigInteger sharedSecret, secretLong, moduloLong, publicKey;
+        publicKey = LongOps.longToBigInt(LongOps.getPublicKey());
+        secretLong = LongOps.longToBigInt(LongOps.getSecretLong());
+        moduloLong = LongOps.longToBigInt(LongOps.getModuloLong());
+        boolean failOrSuccess = LongOps.checkFailure(moduloLong, secretLong,
+                publicKey);
+        System.out.printf("failOrSuccess: %b\n", failOrSuccess);
+        if (failOrSuccess) {
+            System.out.println("BAD_LONG: Error getting one or more long" +
+                    " values");
+            System.exit(-1);
+            sharedSecret = BigInteger.valueOf(-1);
+            return sharedSecret;
+        } else {
+            sharedSecret = publicKey.modPow(secretLong, moduloLong);
+            return sharedSecret;
+        }
+    }
+}
